7fe3c0fe774808ebbfeddfe4572accf084b39c04
Lennart Bank <contact@lencodes.com>
2023-06-29 14:52:31 +0200

    ADD ParsedHTML component

--- pwa/src/templates/landing/LandingTemplate.tsx
+++ pwa/src/templates/landing/LandingTemplate.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
-import Parser from "html-react-parser";
-import Skeleton from "react-loading-skeleton";
 import { Container } from "@conduction/components";
 import { useGitHub } from "../../hooks/gitHub";
-import { getOptions } from "../../services/HtmlParser/getOptions";
+import { ParsedHTML } from "../../components/ParsedHTML/ParsedHTML";
 
 export const LandingTemplate: React.FC = () => {
   const getContent = useGitHub().getContent("/README.md");
 
   return (
     <Container>
-      {getContent.isLoading && <Skeleton height={"200px"} />}
-      {getContent.isSuccess && <div>{Parser(getContent.data, getOptions)}</div>}
+      <ParsedHTML contentQuery={getContent} />
     </Container>
   );
 };
