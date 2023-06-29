36385cc8dc44782b7de43e2a6e3df933bbf702da
Lennart Bank <contact@lencodes.com>
2023-06-29 12:26:48 +0200

    DEPLOY_HOT_FIX

--- pwa/gatsby-config.js
+++ pwa/gatsby-config.js
@@ -1,9 +1,9 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: "static/.env",
 });
 
 module.exports = {
-  pathPrefix: "/product-website-template",
+  pathPrefix: `/${process.env.GITHUB_REPOSITORY_NAME}`,
   plugins: [
     {
       resolve: `gatsby-plugin-layout`,
