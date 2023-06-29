2340d8c42e38c5a5ccbd6fae86c8e598c66474ba
Lennart Bank <contact@lencodes.com>
2023-06-29 16:17:59 +0200

    FIX

--- pwa/src/apiService/apiService.ts
+++ pwa/src/apiService/apiService.ts
@@ -19,7 +19,7 @@ export type TSendFunction = (
 export default class APIService {
   public get gitHubClient(): AxiosInstance {
     return axios.create({
-      baseURL: window.sessionStorage.getItem("GITHUB_API_BASE_URL") ?? "",
+      baseURL: window.sessionStorage.getItem("GITHUB_API_BASE_URL") ?? "NO_URL_FOUND",
       headers: {
         Accept: "application/vnd.github.html",
       },
