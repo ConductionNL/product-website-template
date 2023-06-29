03b7915b8c56a4a4d85070f28c016d68dbbfea21
lencodes <66552932+lencodes@users.noreply.github.com>
2023-06-29 13:51:38 +0000

    Deploying to gh-pages from @ ConductionNL/product-website-template@11a863f85a735ef8d293360d511b37152e5b9154 🚀

---
+++ env.js
@@ -0,0 +1,19 @@
+window.sessionStorage.setItem(
+  "GITHUB_REPOSITORY_URL",
+  "https://github.com/CommonGateway/CoreBundle"
+);
+
+window.sessionStorage.setItem(
+  "SLACK_URL",
+  "https://join.slack.com/t/conductionworkspace/shared_invite/zt-1y5kerime-amWEvljvnLvo5S3YfU_jzg"
+);
+
+window.sessionStorage.setItem(
+  "READ_THE_DOCS_URL",
+  "https://commongateway.readthedocs.io/en/latest/"
+);
+
+window.sessionStorage.setItem(
+  "GITHUB_API_BASE_URL",
+  "https://api.github.com/repos/CommonGateway/CoreBundle/contents"
+);
