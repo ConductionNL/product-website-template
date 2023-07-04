b65a6c6fad019c91178543f6afb532e3986e19b2
remko48 <43807324+remko48@users.noreply.github.com>
2023-07-04 13:01:03 +0000

    Deploying to gh-pages from @ ConductionNL/product-website-template@f480b0390b9b6a9afe5697aec24e8c293e9b3b03 🚀

--- component---src-pages-features-index-tsx-0a0f94ae441e22459e6c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproduct_website_template=self.webpackChunkproduct_website_template||[]).push([[412,292],{4316:function(e,t,u){u.r(t);var a=u(7294),c=u(1082),f=u(5893);t.default=()=>(a.useEffect((()=>{(0,c.c4)("/")})),(0,f.jsx)(f.Fragment,{}))},4894:function(e,t,u){u.r(t);var a=u(4316);t.default=a.default}}]);
-//# sourceMappingURL=component---src-pages-features-index-tsx-0a0f94ae441e22459e6c.js.map
---
+++ component---src-pages-features-index-tsx-a64eb2febf42f4833d10.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproduct_website_template=self.webpackChunkproduct_website_template||[]).push([[412,292],{9450:function(t,e,n){n.r(e),n.d(e,{default:function(){return o}});var u=n(6670),r=n(6981),s=n(7842),c=n(5893);const a=()=>{const t=(0,r.H)().getContent("/docs/features/README.md");return(0,c.jsx)(u.W2,{children:(0,c.jsx)(s.C,{contentQuery:t})})};var o=()=>(0,c.jsx)(a,{})},4894:function(t,e,n){n.r(e);var u=n(9450);e.default=u.default}}]);
+//# sourceMappingURL=component---src-pages-features-index-tsx-a64eb2febf42f4833d10.js.map
