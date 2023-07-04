8990cc8061d3087498bde2fbd892234d789264bb
Remko <remko@conduction.nl>
2023-07-04 09:02:17 +0200

    cleanup

--- pwa/src/templates/templateParts/header/HeaderTemplate.tsx
+++ pwa/src/templates/templateParts/header/HeaderTemplate.tsx
@@ -2,20 +2,8 @@ import * as React from "react";
 import * as styles from "./HeaderTemplate.module.css";
 import { Container } from "@conduction/components";
 import { TopNav } from "../topNav/TopNav";
-import { JumbotronTemplate } from "../../jumbotronTemplate/JumbotronTemplate";
-import { useGatsbyContext } from "../../../context/gatsby";
 
 export const HeaderTemplate: React.FC = () => {
-  const { gatsbyContext } = useGatsbyContext();
-  const [pathname, setPathname] = React.useState<string>("");
-  const [windowExist, setWindowExist] = React.useState<boolean>(true);
-
-  React.useEffect(() => {
-    if (!gatsbyContext) return;
-
-    setPathname(gatsbyContext.location?.pathname);
-  }, [gatsbyContext]);
-
   return (
     <>
       <div className={styles.container}>
