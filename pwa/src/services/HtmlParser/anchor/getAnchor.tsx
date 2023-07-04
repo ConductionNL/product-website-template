1376a72782bdda0e5a05f36962f84e222d0dd033
Lennart Bank <contact@lencodes.com>
2023-07-04 13:37:13 +0200

    INIT

--- pwa/src/services/HtmlParser/anchor/getAnchor.tsx
+++ pwa/src/services/HtmlParser/anchor/getAnchor.tsx
@@ -1,21 +1,43 @@
 import { Link } from "@utrecht/component-library-react/dist/css-module";
+import { navigate } from "gatsby";
 import { domToReact } from "html-react-parser";
 
 export const getAnchor = (props: any, children: any, getOptions: any) => {
+  const currentPath = window.location.pathname;
   const _id = props.id?.replace("user-content-", "");
+
+  console.log({ props });
 
   const handleClick = (e: any) => {
     e.preventDefault();
 
-    if (props.className === "anchor") {
+    if (!props.href) {
+      navigate("#");
+
+      return;
+    }
+
+    if (props.className === "anchor" || Array.from(props.href)[0] === "#") {
       handleAnchorClick(); // handles on-page scroll anchors
+
+      return;
+    }
+
+    if (props.href.includes("https" || "http")) {
+      open(props.href); // external link
+
+      return;
+    }
+
+    if (!props.href.includes("https" || "http")) {
+      handleRelativeLink();
 
       return;
     }
   };
 
   const handleAnchorClick = () => {
-    const target = document.getElementById(_id);
+    const target = document.getElementById(_id ?? props.href.replace("#", ""));
     const headerHeight = document.getElementById("header")?.clientHeight ?? 100;
 
     if (target) {
@@ -23,10 +45,22 @@ export const getAnchor = (props: any, children: any, getOptions: any) => {
     }
   };
 
+  const handleRelativeLink = () => {
+    // linking to a feature, from a feature
+    if (!props.href.includes("/") && currentPath.includes("features")) {
+      navigate(`/features/${props.href.replace(".md", "")}`);
+
+      return;
+    }
+
+    open(`${window.sessionStorage.getItem("GITHUB_REPOSITORY_URL")}/tree/master${props.href}`);
+  };
+
   const attributes = {
     ...props,
     id: _id,
     onClick: handleClick,
   };
+
   return <Link {...attributes}>{domToReact(children, getOptions)}</Link>;
 };
