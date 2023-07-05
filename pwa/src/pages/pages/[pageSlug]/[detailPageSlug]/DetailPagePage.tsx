d72bf8b0996e47babee6bb7a840beb73d8d75c1c
Lennart Bank <contact@lencodes.com>
2023-07-05 10:48:49 +0200

    REMOVE old Features pages and templates

--- pwa/src/pages/pages/[pageSlug]/[detailPageSlug]/DetailPagePage.tsx
+++ pwa/src/pages/pages/[pageSlug]/[detailPageSlug]/DetailPagePage.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import { PageProps } from "gatsby";
 import { DetailPageTemplate } from "../../../../templates/detailPageTemplate/DetailPageTemplate";
 
-const FeatureDetailPage: React.FC<PageProps> = (props: PageProps) => {
+const DetailPagePage: React.FC<PageProps> = (props: PageProps) => {
   const detailPageSlug = props.params.detailPageSlug;
   const pageSlug = props.params.pageSlug;
 
   return <DetailPageTemplate {...{ pageSlug, detailPageSlug }} />;
 };
 
-export default FeatureDetailPage;
+export default DetailPagePage;
