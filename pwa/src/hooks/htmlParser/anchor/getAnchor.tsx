315db1286a3ee11ea1e1792ea7432ed1e7f15c99
Lennart Bank <contact@lencodes.com>
2023-07-05 14:29:36 +0200

    ADD anchor links

--- pwa/src/hooks/htmlParser/anchor/getAnchor.tsx
+++ pwa/src/hooks/htmlParser/anchor/getAnchor.tsx
@@ -15,52 +15,26 @@ export const getAnchor = (
     e.preventDefault();
 
     const targetFile = _.upperFirst(props.href.substring(props.href.lastIndexOf("/") + 1).replace(".md", ""));
-    /**
-     * Internal links
-     */
+
+    // Anchor Links
+    if (props.className === "anchor" || Array.from(props.href)[0] === "#") {
+      handleAnchorClick(props); // handles on-page scroll anchors
+
+      return;
+    }
+
+    // Internal Links
     if (!props.href.includes("://")) {
-      /**
-       * Internal Links: same directory
-       */
-      if (!props.href.includes("/")) {
-        const targetDirectory = _.upperFirst(location.split("/").reverse()[1]);
-        navigate(`/pages/${targetDirectory}/${targetFile}`);
+      handleInternalLinks(props, targetFile, location, directories);
 
-        return; // ensure no other flow is triggered
-      }
+      return;
+    }
 
-      /**
-       * Internal Links: homepage
-       */
-      if (props.href.includes("/") && location === "/") {
-        const directoryFound = directories.some((directory) => directory.location === props.href);
+    // External Links
+    if (props.href.includes("://")) {
+      open(props.href);
 
-        if (directoryFound) navigate(`/pages/${targetFile}`);
-
-        return;
-      }
-
-      /**
-       * Internal Links: different directory
-       */
-      if (props.href.includes("/")) {
-        const targetDirectory = props.href.split("/").reverse()[1];
-        const directoryFound = directories.some(
-          (directory) => directory.location.substring(directory.location.lastIndexOf("/") + 1) === targetDirectory,
-        );
-
-        // Internal Link exists: redirect to page
-        if (directoryFound) {
-          navigate(`/pages/${targetDirectory}/${targetFile}`);
-        }
-
-        // Internal Link does not exist: redirect to online GitHub environment (TODO)
-        if (!directoryFound) {
-          open("https://github.com");
-        }
-
-        return; // ensure no other flow is triggered
-      }
+      return;
     }
   };
 
@@ -70,4 +44,54 @@ export const getAnchor = (
   };
 
   return <Link {...attributes}>{domToReact(children, options)}</Link>;
+};
+
+const handleInternalLinks = (props: any, targetFile: string, location: string, directories: TGitHubDirectory[]) => {
+  // Internal Links: same directory
+  if (!props.href.includes("/")) {
+    const targetDirectory = _.upperFirst(location.split("/").reverse()[1]);
+    navigate(`/pages/${targetDirectory}/${targetFile}`);
+
+    return; // ensure no other flow is triggered
+  }
+
+  // Internal Links: homepage
+  if (props.href.includes("/") && location === "/") {
+    const directoryFound = directories.some((directory) => directory.location === props.href);
+
+    if (directoryFound) navigate(`/pages/${targetFile}`);
+
+    return; // ensure no other flow is triggered
+  }
+
+  // Internal Links: different directory
+  if (props.href.includes("/")) {
+    const targetDirectory = props.href.split("/").reverse()[1];
+    const directoryFound = directories.some(
+      (directory) => directory.location.substring(directory.location.lastIndexOf("/") + 1) === targetDirectory,
+    );
+
+    // Internal Link exists: redirect to page
+    if (directoryFound) {
+      navigate(`/pages/${targetDirectory}/${targetFile}`);
+    }
+
+    // Internal Link does not exist: redirect to online GitHub environment (TODO)
+    if (!directoryFound) {
+      open("https://github.com");
+    }
+
+    return; // ensure no other flow is triggered
+  }
+};
+
+const handleAnchorClick = (props: any) => {
+  const targetId = props.id ?? props.href.replace("#", "user-content-");
+
+  const target = document.getElementById(targetId);
+  const headerHeight = document.getElementById("header")?.clientHeight ?? 100;
+
+  if (target) {
+    window.scrollTo({ top: target.offsetTop - (headerHeight + 24), behavior: "smooth" }); // +24 simply adds some padding
+  }
 };
