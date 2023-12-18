8ada21027a04f1db2977ca940b66cb5a081c1869
Remko <remko@conduction.nl>
2023-12-18 14:39:18 +0100

    cleanup

--- pwa/src/layout/Head.tsx
+++ pwa/src/layout/Head.tsx
@@ -7,7 +7,7 @@ import { useGatsbyContext } from "../context/gatsby";
 
 export const Head: React.FC = () => {
   const { gatsbyContext } = useGatsbyContext();
-  const theme = "zutphen-theme" ?? "conduction-theme";
+  const theme = process.env.GATSBY_NL_DESIGN_THEME_CLASSNAME ?? "conduction-theme";
 
   return (
     <Helmet
