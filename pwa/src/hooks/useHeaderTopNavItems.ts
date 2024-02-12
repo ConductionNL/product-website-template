4f877f56469b09588f4d989930be27db0eb6e39a
Remko <remko@conduction.nl>
2024-02-12 11:26:14 +0100

    Home fix

--- pwa/src/hooks/useHeaderTopNavItems.ts
+++ pwa/src/hooks/useHeaderTopNavItems.ts
@@ -46,7 +46,7 @@ export const useHeaderTopNavItems = (optionalData?: THeaderTopNavItem[]) => {
         const prefixedPathname =
           process.env.GATSBY_USE_GITHUB_REPOSITORY_NAME_AS_PATH_PREFIX === "true"
             ? `/${process.env.GATSBY_GITHUB_REPOSITORY_NAME}${current.pathname}`
-            : `${current.pathname}/`;
+            : `${current.pathname}${current.pathname !== "/" ? "/" : ""}`;
 
         const isCurrentRoute = (): boolean => {
           if (prefixedPathname === gatsbyContext.location.pathname) return true;
